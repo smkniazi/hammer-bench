abb31f4d9def9e343fa106b3890ac245bc4e0530
Salman Niazi <salman@sics.se>
2016-08-05 14:42:13 +0200

    fixed os-type.sh script for centos

--- scripts/os-type.sh
+++ scripts/os-type.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-os=$(grep  "^NAME=" "/etc/os-release" | cut -d'=' -f 2 | cut -d'"' -f 2)
-if [ $os == "Ubuntu" ]; then
+#!/bin/sh
+os=$(grep  "^NAME=" "/etc/os-release" | cut -d'=' -f 2 | cut -d '"' -f 2)
+if [ "$os" == "Ubuntu" ]; then
  echo "Ubuntu"
-elif [ $os == "CentOS Linux" ]; then
+elif [ "$os" == "CentOS Linux" ]; then
  echo "CentOS"
 else
  echo $os
 fi
+
