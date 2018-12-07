529dcb155880ddfd5830bd124bbd28dea72a2ab8
Mahmoud Ismail <maism@hops.io>
2018-12-07 16:00:22 +0100

    Truncate tables before format

--- scripts/internals/formatNN.sh
+++ scripts/internals/formatNN.sh
@@ -6,6 +6,16 @@
 
 #load config parameters
 
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_inodes"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_block_infos"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_block_lookup_table"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_replicas"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_inmemory_file_inode_data"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_ondisk_small_file_inode_data"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_ondisk_medium_file_inode_data"
+/srv/hops/mysql-cluster/ndb/scripts/mysql-client.sh hops -e "truncate table hdfs_ondisk_large_file_inode_data"
+#/home/tester/.mysql/mysql-cluster-gpl-7.5.5-linux-glibc2.5-x86_64/bin/mysql -uroot hop_salman_sf -e "truncate table bench_blockreporting_datanodes"
+
 
 connectStr="$HopsFS_User@$Current_Leader_NN"
 #echo "Drop Schema and Recreate  ...  on $Current_Leader_NN"
