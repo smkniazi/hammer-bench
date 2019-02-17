cfece69494b67f1c27d964270b9857d3d65e0e8e
Salman Niazi <smkniazi@kth.se>
2019-02-17 20:30:02 +0100

    Auto generated commit msg.

--- scripts/internals/formatNN.sh
+++ scripts/internals/formatNN.sh
@@ -7,10 +7,26 @@
 #load config parameters
 
 
+truncateTable() {
+  tableName=$1
+  echo "Truncating $tableName table"
+  /home/tester/.mysql/mysql/bin/mysql -u root hop_salman -e "truncate table $tableName"
+}
+
+
+#truncate speeds up the format if there is lot of data the following tables
+truncateTable hdfs_inodes
+truncateTable hdfs_block_infos
+truncateTable hdfs_block_lookup_table
+truncateTable hdfs_replicas
+truncateTable hdfs_inmemory_file_inode_data
+truncateTable hdfs_ondisk_small_file_inode_data
+truncateTable hdfs_ondisk_medium_file_inode_data
+truncateTable hdfs_ondisk_large_file_inode_data
+truncateTable bench_blockreporting_datanodes
+
+
 connectStr="$HopsFS_User@$Current_Leader_NN"
-#echo "Drop Schema and Recreate  ...  on $Current_Leader_NN"
-#ssh $connectStr $HopsFS_Remote_Dist_Folder/bin/hdfs namenode -dropAndCreateDB
-#ssh $connectStr $HopsFS_Remote_Dist_Folder/sbin/drop-and-recreate-hops-db.sh
 echo "Formatting ...  on $Current_Leader_NN"
 ssh $connectStr $HopsFS_Remote_Dist_Folder/bin/hdfs namenode -format 
 
@@ -20,7 +36,3 @@ source ./internals/wipe-datanode-data-dir.sh
 
 echo "Deleting log files"
 source ./internals/wipe-nn-logs.sh
-
-
-
-
