81a0693960a34c1f86a31e61ef38bec00a5075e0
Mahmoud Ismail <maism@hops.io>
2019-04-24 14:30:40 +0200

    fixes

--- src/main/java/io/hops/experiments/results/compiler/CephMDSAggregator.java
+++ src/main/java/io/hops/experiments/results/compiler/CephMDSAggregator.java
@@ -44,7 +44,8 @@ public class CephMDSAggregator {
   List<String> EXPS = Arrays.asList("default", "pining", "skipKernel-pining");
   
   public static void main(String[] args) throws Exception {
-    new CephMDSAggregator().processSpotify(args[0]);
+    new CephMDSAggregator().processSpotify("/Users/maism/Google " +
+        "Drive/Work/KTH/HopsFS Cloud Paper/results/cephfs/results/spotify");
   }
   
   
@@ -86,6 +87,16 @@ public class CephMDSAggregator {
         DescriptiveStatistics stats = new DescriptiveStatistics();
   
         DescriptiveStatistics jlatstats = new DescriptiveStatistics();
+  
+        DescriptiveStatistics createlat = new DescriptiveStatistics();
+        DescriptiveStatistics lookuplat = new DescriptiveStatistics();
+        DescriptiveStatistics mkdirlat = new DescriptiveStatistics();
+        DescriptiveStatistics readdirlat = new DescriptiveStatistics();
+        DescriptiveStatistics renamelat = new DescriptiveStatistics();
+        DescriptiveStatistics unlinklat = new DescriptiveStatistics();
+        DescriptiveStatistics openlat = new DescriptiveStatistics();
+  
+        DescriptiveStatistics avgreplylat = new DescriptiveStatistics();
         
         List<File> files = CompileResults.findFiles(expDir, "-perf");
         Collections.sort(files, new Comparator<File>() {
@@ -98,14 +109,30 @@ public class CephMDSAggregator {
         for (File file : files) {
           JSONObject json = new JSONObject(FileUtils.readFileToString(file));
           if (json.has("mds_server")) {
-            int requests = json.getJSONObject("mds_server").getInt(
+            JSONObject mdsServer = json.getJSONObject("mds_server");
+            int requests = mdsServer.getInt(
                 "handle_client_request");
             stats.addValue(requests);
+            
+            createlat.addValue(mdsServer.getJSONObject("req_create_latency").getDouble("avgtime"));
+            lookuplat.addValue(mdsServer.getJSONObject("req_lookup_latency").getDouble("avgtime"));
+            mkdirlat.addValue(mdsServer.getJSONObject("req_mkdir_latency").getDouble("avgtime"));
+            readdirlat.addValue(mdsServer.getJSONObject("req_readdir_latency").getDouble("avgtime"));
+            renamelat.addValue(mdsServer.getJSONObject("req_rename_latency").getDouble("avgtime"));
+            unlinklat.addValue(mdsServer.getJSONObject("req_unlink_latency").getDouble("avgtime"));
+            openlat.addValue(mdsServer.getJSONObject("req_open_latency").getDouble("avgtime"));
           }
           if(json.has("mds_log")){
             double journal_flush_latency =
                 json.getJSONObject("mds_log").getJSONObject("jlat").getDouble("avgtime") * 1000;
             jlatstats.addValue(journal_flush_latency);
+          }
+  
+          if(json.has("mds")){
+            double replyLat =
+                json.getJSONObject("mds").getJSONObject("reply_latency").getDouble(
+                    "avgtime");
+            avgreplylat.addValue(replyLat);
           }
         }
         
@@ -129,7 +156,10 @@ public class CephMDSAggregator {
         
         expResults.put(dp, Arrays.asList(result.getSpeed(), mdsSpeed,
             klDivergence(normalized.getValues(), meanArr),
-            result.getAvgOpLatency(), jlatstats.getMean()));
+            result.getAvgOpLatency(), jlatstats.getMean(),
+            createlat.getMean(), lookuplat.getMean(),
+            mkdirlat.getMean(), readdirlat.getMean(), renamelat.getMean(),
+            unlinklat.getMean(), avgreplylat.getMean()));
   
         expbalance.put(dp, normalized);
       }
@@ -160,6 +190,38 @@ public class CephMDSAggregator {
     }
     
     System.out.println(sb.toString());
+    
+    System.out.println("\n\n");
+    
+    sb = new StringBuilder();
+    sb.append("#MDSS " + EXPS.toString() + "\n");
+    sb.append("#MDSS create lookup mkdir readdir rename unlink avg\n");
+    for (int dp : EXTENDED_DATAPOINTS) {
+      sb.append(dp + " ");
+    
+      for(String exp : EXPS) {
+      
+        if(allResults.get(exp).containsKey(dp)) {
+          List<Double> res = allResults.get(exp).get(dp);
+          
+         // double avg = 0;
+          for(int i=5; i<=11; i++){
+            sb.append((res.get(i) * 1000) + " ");
+            //avg+=res.get(i);
+          }
+          
+         // avg /= 6;
+         // sb.append((avg * 1000) + " ");
+        }else{
+          sb.append(" - - - - - - -");
+        }
+      }
+      sb.append("\n");
+    }
+  
+    System.out.println(sb.toString());
+  
+    System.out.println("\n\n");
     
     //show imbalance fo a a datapoint
     sb = new StringBuilder();
