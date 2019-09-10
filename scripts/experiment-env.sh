c0723f8a5b59ae402443c999a3ca8f981e6d063d
Salman Niazi <salman@logicalclocks.com>
2019-09-10 20:56:39 +0200

    Auto generated commit msg.

--- scripts/experiment-env.sh
+++ scripts/experiment-env.sh
@@ -40,8 +40,8 @@ TINY_DATANODES_PER_NAMENODE=5                      #No of simulated datanodes fo
 #experiments to run
 #NOTE all experiment related parameters are in master.properties file
 Benchmark_Types=(
-          #  RAW                                         #Test raw throughput of individual operations
-            INTERLEAVED                                  #Test synthetic workload from spotify 
+            RAW                                         #Test raw throughput of individual operations
+            #INTERLEAVED                                  #Test synthetic workload from spotify 
             #BR                                          #Block report testing. Set the hart beat time for the datanodes to Long.MAX_VALUE. We use a datanode class that does not send HBs  
             ) #space is delimeter
 
