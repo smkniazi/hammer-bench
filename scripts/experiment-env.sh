dae76112fd6a137d74ba5c0ac97a542fc9ebb2a3
Salman Niazi <salman@logicalclocks.com>
2019-09-10 20:54:12 +0200

    Auto generated commit msg.

--- scripts/experiment-env.sh
+++ scripts/experiment-env.sh
@@ -32,7 +32,7 @@ NNS_FullList=(`grep -v "^#" namenodes`)
 NDB_FullList=(`grep -v "^#" ndb-datanodes`)
 
 BM_Machines_FullList=(`grep -v "^#" experiment-nodes`)      #These are the machines that run the benchmark application. Basically, these machines are containers for DFSClients. 
-DFS_CLIENTS_PER_NAMENODE=900                         #In RAW and INTERLEAVED benchmarks use DFS_CLIENTS_PER_NAMENODE*(No of active namenodes in the experiment) clients to stress the namenodes.
+DFS_CLIENTS_PER_NAMENODE=100                         #In RAW and INTERLEAVED benchmarks use DFS_CLIENTS_PER_NAMENODE*(No of active namenodes in the experiment) clients to stress the namenodes.
                                                    #These clients are uniformly distributed among the benchmark (BM_Machines_FullList) machines. 
                                                    #if DFS_CLIENTS_PER_NAMENODE=1000, 5 namenodes and two benchmark machines (BM_Machines_FullList) then each benchmark machine will have 2500 DFSClients
 TINY_DATANODES_PER_NAMENODE=5                      #No of simulated datanodes for benchmarking the blockreporting system
